e331cd8b8ab324c3347d0763443d6e11bcd42161
anishkarki <anish.karki3579@gmail.com>
2018-09-09 16:44:26 +0545

    This is git

---
+++ mysite/blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    text=models.CharField(max_length=1000)
+    pub_date=models.DateTimeField('date published')
+    comment=models.CharField(max_length=1000)
