3775219d859e73046d903eace453d87eaf7311f5
swikarsharma <closetheshit@gmail.com>
2018-09-09 17:43:24 +0545

    ehat

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog',
+    'blog',t
 ]
 
 ROOT_URLCONF = 'mysite.urls'
